0c3639837b6220f2bc6f3ad151066fe628fdcb7b
soumyabrata <soumyabratam6@gmail.com>
2024-08-08 06:46:40 +0530

    all file add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className="App">
+      <h1>React Tooltip Example</h1>
+      <div>
+      <Tooltip text="This is a tooltip for the first element">
+        <h1 className="highlight">Hover over me!</h1>
+      </Tooltip>
+      </div>
+      <div className="tooltip2">
+      <Tooltip text="This is a tooltip for the second element">
+        <p className="highlight">Hover me to see another tootip</p>
+      </Tooltip>
+      </div>
+    </div>
     </div>
   )
 }
