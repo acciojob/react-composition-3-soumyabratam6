70226b045a20a2cc21297f21fbfcb208bf3d3900
soumyabrata <soumyabratam6@gmail.com>
2024-08-08 07:27:07 +0530

    app.js fix and add

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,12 @@ const App = () => {
   return (
     <div>
         <div className="App">
-      <h1>React Tooltip Example</h1>
-      <div>
-      <h2><Tooltip text="This is a tooltip for the first element">
-        <h1 className="highlight">Hover over me!</h1>
-      </Tooltip></h2>
-      </div>
-      <div className="tooltip2">
-      <p><Tooltip text="This is a tooltip for the second element">
-        <p className="highlight">Hover me to see another tootip</p>
-      </Tooltip></p>
-      </div>
+        <Tooltip text="This is a tooltip for the first element">
+    <h2 className="tooltip highlight">Hover over me!</h2>
+</Tooltip>
+<Tooltip text="This is a tooltip for the second element">
+    <p className="tooltip highlight">Hover me to see another tooltip</p>
+</Tooltip>
     </div>
     </div>
   )
